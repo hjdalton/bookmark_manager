69bfca2a24e26db570f599a4dc5582f92c03384c
hjdalton <hjdalton@hotmail.co.uk>
2021-08-09 16:11:28 +0100

    feature test update

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -1,7 +1,10 @@
 feature 'bookmark test' do 
   scenario 'failing test' do 
     visit('/bookmarks')
-    expect(page).to have_content "Bookmark"
+    
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.getrichquick.com"
+    expect(page).to have_content "http://www.google.com"
   end
 end
 
